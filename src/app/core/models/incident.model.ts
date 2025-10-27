c8242cc46d80ced0a2fabf951eb5d74719821049
Matias Armestar <102616485+MatiasArmestar@users.noreply.github.com>
2025-10-26 22:46:06 -0500

    fix: correct Incident model

--- src/app/core/models/incident.model.ts
+++ src/app/core/models/incident.model.ts
@@ -1,14 +1,11 @@
 export type IncidentState = 'Abierto' | 'En Proceso' | 'Cerrado';
 
 export interface Incident {
-  id: number;
-  empresaId: number;
-  contratoId: number;
-  code: string;        // "INC-101"
-  serie: string;
-  modelo: string;
-  fecha: string;       // YYYY-MM-DD
-  estado: IncidentState;
-  descripcion: string;
-  tecnico: string;
+  "id": number,
+  "userId": number,
+  "serial": string,
+  "model": string,
+  "date": string,
+  "state": IncidentState,
+  "description": string
 }
