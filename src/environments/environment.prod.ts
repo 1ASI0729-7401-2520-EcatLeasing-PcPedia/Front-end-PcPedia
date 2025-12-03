8379b798ec5cb81ba519ecbcf9997beaf51cd351
M1sterDG <sebastianehp@gmail.com>
2025-12-03 01:04:56 -0500

    fix: conection from backend service

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tu-api-backend.com/api',
+  apiUrl: 'https://pcpediaapi-egd4b8frh3bqcsde.canadacentral-01.azurewebsites.net',
   defaultLanguage: 'es',
   supportedLanguages: ['es', 'en']
 };
